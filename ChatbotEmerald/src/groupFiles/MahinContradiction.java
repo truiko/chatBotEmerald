92abdbf1d1cc7726a429503115bcaed2864cd908
Student 8 <Student 8@10.8.33.178>
2016-09-30 13:08:14 -0400

    kikjhg

--- ChatbotEmerald/src/groupFiles/MahinContradiction.java
+++ ChatbotEmerald/src/groupFiles/MahinContradiction.java
@@ -5,7 +5,7 @@ package groupFiles;
 public class MahinContradiction implements Chatbot{
 	private boolean inContradictionLoop;
 	private String contradictionResponse;
-	// a
+	// j
 	public boolean isTriggered(String userInput){
 		if(VictorMain.findKeyword(userInput, "not", 0)>=0){
 			return true;
