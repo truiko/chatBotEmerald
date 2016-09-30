c7f61789810ca654eea636f435b507cd755ae627
Student 8 <Student 8@10.8.33.152>
2016-09-30 12:45:25 -0400

    victor is triggered

---
+++ ChatbotEmerald/src/groupFiles/VictorSignInTopic.java
@@ -0,0 +1,16 @@
+package groupFiles;
+
+
+public class VictorSignInTopic implements Chatbot{
+	public boolean isTriggered(String userInput){
+		if(VictorMain.findKeyword(userInput, "game", 0) >= 0){
+			return true;
+		}
+		return false;
+	}
+
+	public void talk() {
+
+		
+	}
+}
