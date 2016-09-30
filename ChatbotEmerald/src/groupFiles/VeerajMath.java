37b65cfdcc17e53772bc08cb08b93c98b60a0cc9
Ali Student 8 <Student 8@10.8.33.160>
2016-09-30 13:37:27 -0400

    in class

--- ChatbotEmerald/src/groupFiles/VeerajMath.java
+++ ChatbotEmerald/src/groupFiles/VeerajMath.java
@@ -1,8 +1,21 @@
 package groupFiles;
 
 public class VeerajMath implements Chatbot{
+	private boolean inMathLoop;
+	private String mathResponse;
+	
 	public void talk() {
-		
+		inMathLoop = true;
+		while(inMathLoop){
+			VictorMain.print("(Type 'quit' to go back.)");
+			//this is called a static call
+			mathResponse = VictorMain.promptInput();
+			if(mathResponse.indexOf("quit") >= 0){
+				inMathLoop = false;
+				VictorMain.promptForever();
+			}
+			VictorMain.print("What do you want to solve?");
+		}
 	}
 
 	public boolean isTriggered(String userInput) {
