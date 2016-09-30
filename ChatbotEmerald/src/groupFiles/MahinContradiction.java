9a9c61639b478a9849fec1a38064933918c54f44
Student 8 <Student 8@10.8.33.178>
2016-09-30 13:07:11 -0400

    Contradiction things.

---
+++ ChatbotEmerald/src/groupFiles/MahinContradiction.java
@@ -0,0 +1,20 @@
+package groupFiles;
+
+//import chatbot.MahinMain;
+
+public class MahinContradiction implements Chatbot{
+	private boolean inContradictionLoop;
+	private String contradictionResponse;
+	
+	public boolean isTriggered(String userInput){
+		if(VictorMain.findKeyword(userInput, "not", 0)>=0){
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public void talk(){
+		
+	}
+}
