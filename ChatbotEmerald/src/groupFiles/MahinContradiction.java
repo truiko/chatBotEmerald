ac2dcf5482934fec1c60632109299ef54c453af6
Student 8 <Student 8@10.8.33.178>
2016-09-30 13:38:55 -0400

    my changes.

--- ChatbotEmerald/src/groupFiles/MahinContradiction.java
+++ ChatbotEmerald/src/groupFiles/MahinContradiction.java
@@ -10,11 +10,28 @@ public class MahinContradiction implements Chatbot{
 		if(VictorMain.findKeyword(userInput, "not", 0)>=0){
 			return true;
 		}
+		if(VictorMain.findKeyword(userInput, "is", 0)>=0){
+			return true;
+		}
+		if(VictorMain.findKeyword(userInput, "are", 0)>=0){
+			return true;
+		}
+		if(VictorMain.findKeyword(userInput, "am", 0)>=0){
+			return true;
+		}
 		return false;
 		
 	}
 	
 	public void talk(){
-		
+		inContradictionLoop = true;
+		while(inContradictionLoop){
+			//if()
+			//for each user response, if it finds response with similar beginning,
+			//and the rest is different, it will notice.
+			//for each user response, it goes through each word and checks to see
+			//if it matches its recent response. once it notices a mismatch, it says:
+			//"i thought you said:" + theOriginalComment
+		}
 	}
 }
