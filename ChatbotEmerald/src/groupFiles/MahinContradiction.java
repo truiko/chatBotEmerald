23ae472ce9d0539166c0ab9c14ac899e4ffd0dd6
Mahin <mkhan6241@bths.edu>
2016-10-03 18:39:30 -0400

    Changed to calendar.

--- ChatbotEmerald/src/groupFiles/MahinContradiction.java
+++ ChatbotEmerald/src/groupFiles/MahinContradiction.java
@@ -4,20 +4,17 @@ package groupFiles;
 
 
 public class MahinContradiction implements Chatbot{
-	private boolean inContradictionLoop;
-	private String contradictionResponse;
+	private boolean inCalendarLoop;
+	private String calendarResponse;
 	// j
 	public boolean isTriggered(String userInput){
-		if(VictorMain.findKeyword(userInput, "not", 0)>=0){
+		if(VictorMain.findKeyword(userInput, "date", 0)>=0){
 			return true;
 		}
-		if(VictorMain.findKeyword(userInput, "is", 0)>=0){
+		if(VictorMain.findKeyword(userInput, "time", 0)>=0){
 			return true;
 		}
-		if(VictorMain.findKeyword(userInput, "are", 0)>=0){
-			return true;
-		}
-		if(VictorMain.findKeyword(userInput, "am", 0)>=0){
+		if(VictorMain.findKeyword(userInput, "calendar", 0)>=0){
 			return true;
 		}
 		return false;
@@ -25,24 +22,17 @@ public class MahinContradiction implements Chatbot{
 	}
 	
 	public void talk(){
-		inContradictionLoop = true;
-		while(inContradictionLoop){
-			VictorMain.print("Type 'quit' to go back.");
-			contradictionResponse = VictorMain.promptInput();//static call on promptInput method from
+		inCalendarLoop = true;
+		while(inCalendarLoop){
+			VictorMain.print("Would you like to add things to my calendar?");
+			calendarResponse = VictorMain.promptInput();//static call on promptInput method from
 			//MahinMain class
-			if(contradictionResponse.indexOf("quit") >= 0){
-				inContradictionLoop = false;
+			if(calendarResponse.indexOf("no") >= 0){
+				inCalendarLoop = false;
+				VictorMain.print("I don't think you value organization and punctuality.");
 				VictorMain.promptForever();
 			}
-			VictorMain.print("You may have contradicted yourself....");
-			//for each user response, if it finds response with similar beginning,
-			//and the rest is different, it will notice.
-			//for each user response, it goes through each word and checks to see
-			//if it matches its recent response. once it notices a mismatch,
-			//it takes the comment before the new comment or theOriginalComment
-			//(comments that are both similar to each other) and it says:
-			//"i thought you said:" + theOriginalComment
-			//
+			
 		}
 	}
 }
