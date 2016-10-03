2e2e8c39157e37d3298ca7b101b723a1ae89696a
hotfi <hotfi@192.168.1.2>
2016-10-03 18:27:03 -0400

    working rock paper scissor game. will add complexity-veeraj 2k16

--- ChatbotEmerald/src/groupFiles/VictorGame.java
+++ ChatbotEmerald/src/groupFiles/VictorGame.java
@@ -27,23 +27,50 @@ public class VictorGame implements Chatbot{
 			}
 			 if(!isValidChoice(gameResponse)){
 				VictorMain.print("Please choose a valid option.");
+			}else{
+				VictorMain.print(determineWinner(gameResponse));
 			}
 		}
 	}
 	
 	public static String determineWinner(String userChoice){
 		int computerChoice = makeComputerChoice();
+		System.out.println(computerChoice);
 		int ROCK = 1;
 		int PAPER = 2;
 		int SCISSOR = 3;
-		if(userChoice == "rock" && computerChoice == PAPER){
+		if(userChoice.equals("rock") && computerChoice == PAPER){
 			return "The winner is "+ "computer!";
 		}else{
-			if(userChoice == "rock" && computerChoice == SCISSOR){
+			if(userChoice.equals("rock") && computerChoice == SCISSOR){
 				return "The winner is " + VictorMain.user + "!";
 			}else{
-				if(userChoice == "rock" && computerChoice == ROCK){
+				if(userChoice.equals("rock") && computerChoice == ROCK){
 					return "This round is a tie";
+				}else{
+					if(userChoice.equals("paper") && computerChoice == ROCK){
+						return "The winner is " + VictorMain.user + "!";
+					}else{
+						if(userChoice.equals("paper") && computerChoice == SCISSOR){
+							return "The winner is "+ "computer!";
+						}else{
+							if(userChoice.equals("paper") && computerChoice == PAPER){
+								return "This round is a tie";
+							}else{
+								if(userChoice.equals("scissor") && computerChoice == PAPER){
+									return "The winner is " + VictorMain.user + "!";
+								}else{
+									if(userChoice.equals("scissor") && computerChoice == ROCK){
+										return "The winner is "+ "computer!";
+									}else{
+										if(userChoice.equals("scissor") && computerChoice == SCISSOR){
+											return "This round is a tie";
+										}
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 		}
@@ -51,13 +78,15 @@ public class VictorGame implements Chatbot{
 	}
 	
 	private static int makeComputerChoice(){
+		
 		return (int) (Math.random()*3) + 1;
 	}
+	
 	private static boolean isValidChoice(String choice){
 		String[] choices = {"rock", "paper", "scissor"};
 		boolean match = false;
 		for(int i = 0; i < choices.length; i++){
-			if(choice == choices[i]){
+			if(choice.equals(choices[i])){
 				match = true;
 			}
 		}
