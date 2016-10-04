81f943355aa4ca89b8c3c6503fc8194dc8768fb6
Veeraj Jethalal <veera@DESKTOP-0ECFG49>
2016-10-04 02:08:15 -0400

    more work

--- ChatbotEmerald/src/groupFiles/VeerajMath.java
+++ ChatbotEmerald/src/groupFiles/VeerajMath.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class VeerajMath implements Chatbot{
 	private boolean inMathLoop;
-	private String mathQuestion;
 	private String quit2;
 	static Scanner question;
 	static Scanner in;
@@ -15,54 +14,85 @@ public class VeerajMath implements Chatbot{
 	static Scanner quit;
 	private double num1;
 	private double num2;
-	private double result;
 	private String operation;
+	
 	
 	public void talk() {
 		makeFields();
 		inMathLoop = true;
-		VictorMain.print("This is the math section. Do you want to leave?");
-		quit2 = quit.nextLine();
-		quit2 = quit2.toLowerCase();
-		if(VictorMain.findKeyword(quit2, "yes", 0) >= 0 || 
-				VictorMain.findKeyword(quit2, "yea", 0) >= 0){
-			inMathLoop = false;
-			VictorMain.print("I see you don't like math. Fine then.");
-			VictorMain.promptForever();
-		}
-		//this is called a static call
 		while(inMathLoop){
+			VictorMain.print("ONLY USE INTEGERS. Do you want to leave?");
+			quit2 = quit.nextLine();
+			quit2 = quit2.toLowerCase();
+			
+			if(VictorMain.findKeyword(quit2, "yes", 0) >= 0 || 
+				VictorMain.findKeyword(quit2, "yea", 0) >= 0){
+				inMathLoop = false;
+				VictorMain.print("I see you don't like math. Fine then.");
+				VictorMain.promptForever();
+			}
+			
 			VictorMain.print("Enter the first number");
-			num1 = op1.nextDouble();
+			if(getIntegerInput(op1.nextLine())){
+				double a = Double.parseDouble(op1.nextLine());
+				System.out.println(a);
+				num1 = op1.nextDouble();
+			}
+			else{
+				VictorMain.print("Please try again");
+				continue;
+			}
 			
 			VictorMain.print("Enter the second number");
-			num2 = op2.nextDouble();
+			if(getIntegerInput(op2.nextLine())){
+				double b = Double.parseDouble(op2.nextLine());
+				System.out.println(b);
+				num2 = op2.nextDouble();
+			}
 			
 			VictorMain.print("Enter the operation(+, -, *, /)");
 			operation = mid.nextLine();
 			
 			switch (operation){
 				case "+":
-					System.out.println(num1 + num2);
+					add(num1, num2);
 					break;
 				case "-":
-					System.out.println(num1 - num2);
+					subtract(num1, num2);
 					break;
 				case "*":
-					System.out.println(num1 * num2);
+					multiply(num1, num2);
 					break;
 				case "/":
-					System.out.println(num1 / num2);
+					divide(num1, num2);
 					break;
 				default:
-					System.out.println("Please enter a valid operator");
+					VictorMain.print("Please enter a valid operator!");
 			}
 		}
 	}
 	
-	private static int getIntegerInput() {
-		VictorMain.print("Please enter an integer.");
-		String integerString = VictorMain.promptInput();
+	private void add(double one, double two){
+		printResult(one + two);
+		VictorMain.print("It's sad that you can't do simple addition");
+	}
+	
+	private void subtract(double one, double two){
+		printResult(one - two);
+		VictorMain.print("You should go back to elementary school");
+	}
+	
+	private void multiply(double one, double two){
+		printResult(one * two);
+		VictorMain.print("You shouldn't rely on technology to do all the work for you");
+	}
+	
+	private void divide(double one, double two){
+		printResult(one / two);
+		VictorMain.print("I'm smarter than you!"); 
+	}
+	
+	private static boolean getIntegerInput(String integerString) {
 		boolean isInteger = false;
 		int value = 0;
 		while(!isInteger){
@@ -73,11 +103,10 @@ public class VeerajMath implements Chatbot{
 			 }
 		 
 			 catch(NumberFormatException e){
-				VictorMain.print("You must enter an integer. You better try again.");
-				integerString = VictorMain.promptInput();
+				isInteger = false;
 			 }
-		 }
-		 	return value;
+			}
+			return isInteger;
 		 }
 
 	public boolean isTriggered(String userInput) {
@@ -87,17 +116,16 @@ public class VeerajMath implements Chatbot{
 		return false;
 	}
 	
-	public void makeFields(){
+	private void printResult(double input){
+		System.out.println(input);
+	}
+	
+	private void makeFields(){
 		question = new Scanner(System.in);
 		quit = new Scanner(System.in);
 		op1 = new Scanner(System.in);
 		op2 = new Scanner(System.in);
 		res = new Scanner(System.in);
 		mid = new Scanner(System.in);
-		String mathQuestion = "";
-		String quit2 = "";
-		double num1;
-		double num2;
-		double result;
 	}
 }
