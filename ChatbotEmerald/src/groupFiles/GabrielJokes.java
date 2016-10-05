d6b75af58533ec7af6b5e51f807fdeba03b7b214
Student 8 <Student 8@10.8.33.173>
2016-10-05 14:03:14 -0400

    Worked on talk method.

--- ChatbotEmerald/src/groupFiles/GabrielJokes.java
+++ ChatbotEmerald/src/groupFiles/GabrielJokes.java
@@ -1,10 +1,15 @@
 package groupFiles;
 
+import java.util.Random;
+
 public class GabrielJokes implements Chatbot {
 	
 	private boolean inJokeLoop;
 	private String jokeResponse;
-
+	private int jokeCount;
+	private String[] jokes = {"Amos", "Etch", "Cows go"};
+	private String [] jokeAnswers = {"A mosquito", "Bless you", "No silly. Cows go moo"};
+	
 	public boolean isTriggered(String userInput) {
 
 		if(VictorMain.findKeyword(userInput, "funny", 0) >= 0){
@@ -14,15 +19,27 @@ public class GabrielJokes implements Chatbot {
 	}
 
 	public void talk() {
-		inJokeLoop = true;
-		while(inJokeLoop){
-			VictorMain.print("Would you like to hear a joke?");
-			jokeResponse = VictorMain.promptInput();
-			if(jokeResponse.indexOf("no") >= 0){
-				inJokeLoop = false;
-				VictorMain.print("Fine then");
-				VictorMain.promptForever();
+		
+		VictorMain.print("Would you like to hear a joke?");
+		jokeResponse = VictorMain.promptInput();
+		jokeResponse = jokeResponse.toLowerCase();
+		if(VictorMain.findKeyword(jokeResponse, "no", 0)>= 0){
+			inJokeLoop = false;
+			VictorMain.print("Fine then");
+			VictorMain.promptForever();
+		}
+		if((VictorMain.findKeyword(jokeResponse, "yeah", 0)>= 0) || (VictorMain.findKeyword(jokeResponse, "yes", 0) >= 0)){
+			VictorMain.print("Knock knock.");				
+			inJokeLoop = true;
+			while(inJokeLoop){
+				String randomJoke = (jokes[new Random().nextInt(jokes.length)]);
+				if(VictorMain.findKeyword(jokeResponse, "who's there", 0) >= 0){
+					VictorMain.print("randomJoke");
+				}
+							
 			}
 		}
 	}
-}
+}	
+
+
