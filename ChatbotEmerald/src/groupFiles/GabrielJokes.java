ebb5f5e5d55969f99a41abd99a89030bb845a027
User <User@Acer>
2016-10-05 21:28:21 -0400

    working on conditionals and bullet proofing

--- ChatbotEmerald/src/groupFiles/GabrielJokes.java
+++ ChatbotEmerald/src/groupFiles/GabrielJokes.java
@@ -4,14 +4,16 @@ import java.util.Random;
 
 public class GabrielJokes implements Chatbot {
 	
+	private String jokeChoice;
 	private boolean inJokeLoop;
 	private String jokeResponse;
 	private int jokeCount;
-	private String[] jokes = {"Amos", "Etch", "Cows go"};
-	private String [] jokeAnswers = {"A mosquito", "Bless you", "No silly. Cows go moo"};
+	
+	static int jokeIndex = 0;
+	static String[] jokes = {"Amos","Etch","Cows go"};
+	static String[] jokeAnswers = {"A mosquito","Bless you","No silly. Cows go moo"};
 	
 	public boolean isTriggered(String userInput) {
-
 		if(VictorMain.findKeyword(userInput, "funny", 0) >= 0){
 			return true;
 		}
@@ -19,27 +21,46 @@ public class GabrielJokes implements Chatbot {
 	}
 
 	public void talk() {
-		
+		inJokeLoop = true;
 		VictorMain.print("Would you like to hear a joke?");
-		jokeResponse = VictorMain.promptInput();
-		jokeResponse = jokeResponse.toLowerCase();
-		if(VictorMain.findKeyword(jokeResponse, "no", 0)>= 0){
-			inJokeLoop = false;
-			VictorMain.print("Fine then");
-			VictorMain.promptForever();
+		while(inJokeLoop){
+			jokeResponse = VictorMain.promptInput();
+			jokeResponse = jokeResponse.toLowerCase();
+			jokeChoice = getJoke();
+			findIndexOfJoke(jokeChoice);
+			
+			if(VictorMain.findKeyword(jokeResponse, "no", 0)>= 0){
+				inJokeLoop = false;
+				VictorMain.print("Fine then");
+				VictorMain.promptForever();
+			}
+			if((VictorMain.findKeyword(jokeResponse, "yeah", 0)>= 0) || (VictorMain.findKeyword(jokeResponse, "yes", 0) >= 0)){
+				VictorMain.print("Knock knock.");	
+			}
+			if(VictorMain.findKeyword(jokeResponse, "there", 0)>= 0){
+				VictorMain.print(jokeChoice);
+			}
+			if(VictorMain.findKeyword(jokeResponse, "who", 0) >= 0){
+				VictorMain.print(jokeAnswers[jokeIndex]);
+			}
+			
 		}
-		if((VictorMain.findKeyword(jokeResponse, "yeah", 0)>= 0) || (VictorMain.findKeyword(jokeResponse, "yes", 0) >= 0)){
-			VictorMain.print("Knock knock.");				
-			inJokeLoop = true;
-			while(inJokeLoop){
-				String randomJoke = (jokes[new Random().nextInt(jokes.length)]);
-				if(VictorMain.findKeyword(jokeResponse, "who's there", 0) >= 0){
-					VictorMain.print("randomJoke"); //hi
-				}
-							
+	}
+	
+	public static String getJoke(){
+		int joke = (int) (Math.random()*jokes.length); 
+		return jokes[joke];
+	}
+	
+	public static void findIndexOfJoke(String joke){
+		for(int i = 0; i < jokes.length; i++){
+			if(jokes[i].equals(joke)){
+				jokeIndex = i;
 			}
 		}
 	}
-}	
+}
+
+	
 
 
